ef48713e1e480db549e4305e8084e865076af434
Ashutosh_tiwari6554843 <ashutoshkumartiwari686@gail.com>
2025-02-11 14:51:48 +0530

    change theam

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  theme: {
+    extend: {
+      colors: {
+        themeWhite: "#FFFFFF", // Custom white
+      },
+    },
+  },
+  darkMode:false,
   plugins: [react(),
     tailwindcss(),
   ],
